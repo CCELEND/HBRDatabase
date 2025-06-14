fc6b3db1ec27e6e0eeda856f440cb6b1f900d187
CCELEND <celend2644884626@163.com>
2025-06-14 23:56:16 +0800

    master

--- 角色/master_skill_win.py
+++ 角色/master_skill_win.py
@@ -6,7 +6,8 @@ from canvas_events import get_photo, create_canvas_with_image
 from window import set_window_expand, creat_Toplevel, set_window_top
 from scrollbar_frame_win import ScrollbarFrameWin
 
-from 角色.style_text import output_skill_effect
+from master_skill_info import MasterSkillEffect
+from style_text import output_skill_effect, output_attack_skill
 import 战斗系统.状态.status_info
 
 # 加载资源文件
@@ -33,7 +34,10 @@ def creat_desc_frame(row_frame, master_skill):
     if master_skill.max_uses:
         text = "SP" + master_skill.sp_cost + '\n' + master_skill.max_uses
     else:
-        text = "SP" + master_skill.sp_cost + '\n' + "∞"
+        if master_skill.sp_cost == "被动技能":
+            text = master_skill.sp_cost + '\n' + "∞"
+        else:
+            text = "SP" + master_skill.sp_cost + '\n' + "∞"
     sp_use_lab = ttk.Label(desc_frame, text=text, 
         justify="right", font=("Monospace", 10, "bold"))
     sp_use_lab.grid(row=0, column=1, sticky="nse", padx=5, pady=5)
@@ -52,12 +56,11 @@ def creat_master_skill_frame(scrollbar_frame_obj, master_skill):
     for col_index in range(4):
         row_frame.grid_columnconfigure(col_index, weight=1)
 
-    # 一个技能的描述 frame
+    # 大师技能的描述 frame
     creat_desc_frame(row_frame, master_skill)
 
-    # 技能效果列表
+    # 技能效果或者攻击技能列表
     for j, skill in enumerate(master_skill.effects):
-
         # 技能效果 frame
         effect_frame = ttk.Frame(row_frame)
         effect_frame.grid(row=j+1, column=0, columnspan=4, pady=(0,5), sticky="nsew")
@@ -66,20 +69,43 @@ def creat_master_skill_frame(scrollbar_frame_obj, master_skill):
         effect_frame.grid_columnconfigure(0, weight=1, minsize=100)  # Canvas 列
         effect_frame.grid_columnconfigure(1, weight=6, minsize=600)  # 右侧信息列，权重更大以填充更多空间
 
-        # 创建技能效果图标 canvas
-        effect_photo = get_photo(战斗系统.状态.status_info.status[skill.effect_type].path, (60, 60))
-        effect_canvas = create_canvas_with_image(effect_frame, 
-            effect_photo, 60, 60, 0, 0, 0, 0)
+        if isinstance(skill, MasterSkillEffect):
+            # 创建技能效果图标 canvas
+            effect_photo = get_photo(战斗系统.状态.status_info.status[skill.effect_type].path, (60, 60))
+            effect_canvas = create_canvas_with_image(effect_frame, 
+                effect_photo, 60, 60, 0, 0, 0, 0)
 
-        text = output_skill_effect(skill.turn_num, skill.duration, skill.target, skill.effect_type,
-            战斗系统.状态.status_info.status[skill.effect_type].description, skill.value, skill.attribute_multiplier,
-            skill.attribute_difference,
-            IsActive=True
-        )
+            text = output_skill_effect(skill.turn_num, skill.duration, skill.target, skill.effect_type,
+                战斗系统.状态.status_info.status[skill.effect_type].description, skill.value, skill.attribute_multiplier,
+                skill.attribute_difference,
+                IsActive=True
+            )
+        # 否则就是攻击技能
+        else:
+            weapon_attribute = skill.weapon_attribute
+            # 判断元素属性
+            if skill.element_attribute:
+                attack_img_path = 战斗系统.属性.attributes_info.attributes[skill.element_attribute+weapon_attribute].path
+            else:
+                attack_img_path = 战斗系统.属性.attributes_info.attributes[weapon_attribute].path
+            # 创建攻击技能图标 canvas
+            attack_photo = get_photo(attack_img_path, (60, 60))
+            attack_canvas = create_canvas_with_image(effect_frame, 
+                attack_photo, 60, 60, 0, 0, 0, 0)
+
+            # 攻击技能信息
+            text = output_attack_skill(skill.hit_num, skill.target, skill.hit_damage, 
+                skill.biased, 
+                skill.strength, skill.attribute_multiplier, 
+                skill.attribute_difference, skill.destructive_multiplier
+            )
 
         desc_lab = ttk.Label(effect_frame, text=text, justify="left", font=("Monospace", 10, "bold"))
         desc_lab.grid(row=0, column=1, sticky="nsw", padx=5, pady=0)
 
+    missions_row = 1 + len(master_skill.effects)
+    missions_lab = ttk.Label(row_frame, text=master_skill.missions , justify="left", font=("Monospace", 10, "bold"))
+    missions_lab.grid(row=missions_row, column=0, sticky="nsw", padx=5, pady=0)
 
 
 # 已打开的大师技能窗口字典，键：风格名，值：窗口句柄
--- 角色/team_win.py
+++ 角色/team_win.py
@@ -9,7 +9,7 @@ from scrollbar_frame_win import ScrollbarFrameWin
 
 from team_info import get_team_obj, get_all_team_obj
 from style_win import creat_style_skill_win, creat_style_right_menu
-# from master_skill_win import creat_master_skill_win
+from master_skill_win import creat_master_skill_win
 
 import 战斗系统.武器.weapons_info
 # 加载资源文件
@@ -160,8 +160,8 @@ def creat_weapon_master_skill_frame(parent_frame, role):
             master_skill_photo, 100, 100, 10, 16, 1, 0)
 
         mouse_bind_canvas_events(master_skill_canvas)
-        # bind_canvas_events(master_skill_canvas, 
-        #     creat_master_skill_win, parent_frame=parent_frame, role=role)
+        bind_canvas_events(master_skill_canvas, 
+            creat_master_skill_win, parent_frame=parent_frame, role=role)
     else:
         weapon_frame = ttk.Frame(parent_frame)
         weapon_frame.grid(row=0, column=2, padx=5, pady=5, sticky="nsew")
