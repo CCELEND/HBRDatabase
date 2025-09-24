bbdd5456114c3154b8ce8cf8c5ef6beb735b0bc4
CCELEND <celend2644884626@163.com>
2025-09-24 15:38:08 +0800

    tools music

--- 音乐/music_handle_processing.py
+++ 音乐/music_handle_processing.py
@@ -60,6 +60,7 @@ def get_album_cover_path(all_albun_name, file_name):
         file_name = file_name.replace("03.陽のさす向こうへ", "02.春眠旅団")
         file_name = file_name.replace("11.World We Changed", "02.春眠旅団")
         file_name = file_name.replace("09.複葉機とオールトの雲", "Perfect Smile")
+        file_name = file_name.replace("10.Arch of Light Alternative", "Perfect Smile")
         return album_cover_paths["She_is_Legend"] + file_name.replace("flac", "jpg")
     elif all_albun_name == "Inst_Test_Examples":
         return album_cover_paths["Inst_Test_Examples"] + file_name.replace("flac", "jpg")
