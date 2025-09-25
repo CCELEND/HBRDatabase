21ff43f89509517053bd7d0f8855b0accf5ae624
CCELEND <celend2644884626@163.com>
2025-09-25 17:42:28 +0800

    thread

--- thread_start.py
+++ thread_start.py
@@ -3,5 +3,14 @@ import threading
 
 def creat_new_thread(target_fun):
 	# 创建并启动新线程
-	thread = threading.Thread(target=target_fun)
+	thread = threading.Thread(target=target_fun, daemon=True)
+	thread.start()
+
+def run_in_thread(func):
+
+	def wrapper():
+		func()  # 执行计算任务
+
+	# 创建并启动线程
+	thread = threading.Thread(target=wrapper, daemon=True)
 	thread.start()
