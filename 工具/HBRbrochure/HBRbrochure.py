b0561a3996a7c681ab593c14f416d655c5ff5631
CCELEND <celend2644884626@163.com>
2026-02-11 14:25:20 +0800

    update

--- 工具/HBRbrochure/HBRbrochure.py
+++ 工具/HBRbrochure/HBRbrochure.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pathlib
 
-from tools import init_chrome_driver
+from tools import init_chrome_driver, add_dicts
 # import HBRbrochure.role_info
 import HBRbrochure.brochure
 from 角色.team_info import get_all_team_obj
@@ -105,6 +105,59 @@ def get_limit_break_level(style_item_card_elements, i):
     return limit_break_level
 
 
+def get_styles_info(container):
+
+    # 查找类名为 style-item card 的 <div> 元素
+    card_box_element = container.find_element(
+        By.XPATH, ".//div[contains(@class, 'card-box')]"
+    )
+
+    # 查找类名为 style-item card 的 <div> 元素
+    style_item_card_elements = card_box_element.find_elements(
+        By.XPATH, ".//div[contains(@class, 'style-item') and contains(@class, 'card')]"
+    )
+
+    my_style_num = 0
+    my_style_ids = []
+    my_style_levels = {}
+    limit_break_levels = {}
+    my_style_infos = {}
+
+    # 获取风格数据
+    for i, style_item_card_element in enumerate(style_item_card_elements):
+        # 获取风格 id
+        my_style_id = get_style_id(style_item_card_element)
+        my_style_ids.append(my_style_id)
+
+        # 获取风格等级
+        # result = get_role_level(style_item_card_element)
+        result = get_role_level(style_item_card_elements, i)
+        current_level = result[0]
+        maximum_level = result[1].replace("/", "")  # 去掉第二个元素中的 "/"
+        my_style_levels[my_style_id] = [current_level, maximum_level]
+
+        # 获取风格上限突破
+        limit_break_level = get_limit_break_level(style_item_card_elements, i)
+        limit_break_levels[my_style_id] = limit_break_level
+
+        try:
+            # my_style_infos[my_style_id] = HBRbrochure.role_info.style_id_all_infos[my_style_id]
+            my_style_infos[my_style_id] = {}
+            my_style_infos[my_style_id]["current_level"] = current_level
+            my_style_infos[my_style_id]["maximum_level"] = maximum_level
+            my_style_infos[my_style_id]["limit_break_level"] = limit_break_level
+        except KeyError:
+            logger.error(f"[-] Missing information on style ID, style ID:   {my_style_id}")
+            print("[-] Missing information on style ID, style ID: " + my_style_id)
+            continue
+
+        my_style_num += 1
+
+    
+    return my_style_num, my_style_ids, my_style_levels, limit_break_levels, my_style_infos
+
+
+
 def switch_to_brochure(driver, style_infos):
     # 使用 JavaScript 打开新标签页
     driver.execute_script(
@@ -163,56 +216,40 @@ def run_browser_in_thread():
             EC.visibility_of_element_located((By.CLASS_NAME, "card-box"))
         )
 
-
-        # 查找类名为 style-item card 的 <div> 元素
-        style_item_card_elements = card_box_element.find_elements(
-            By.XPATH, ".//div[contains(@class, 'style-item') and contains(@class, 'card')]"
+        # 等待 class 为 content 的元素加载完成
+        content_element = WebDriverWait(chrome_driver, 300).until(
+            EC.visibility_of_element_located((By.CLASS_NAME, "content"))
         )
 
-        my_style_num = 0
-        my_style_ids = []
-        my_style_levels = {}
-        limit_break_levels = {}
-        my_style_infos = {}
+        container_elements = content_element.find_elements(By.CLASS_NAME, "container")
 
-        # 获取风格数据
-        for i, style_item_card_element in enumerate(style_item_card_elements):
-            # 获取风格 id
-            my_style_id = get_style_id(style_item_card_element)
-            my_style_ids.append(my_style_id)
+        SSR_style_num, SSR_style_ids, SSR_style_levels, SSR_limit_break_levels, SSR_style_infos = get_styles_info(container_elements[0])
+        SS_style_num, SS_style_ids, SS_style_levels, SS_limit_break_levels, SS_style_infos = get_styles_info(container_elements[1])
 
-            # 获取风格等级
-            # result = get_role_level(style_item_card_element)
-            result = get_role_level(style_item_card_elements, i)
-            current_level = result[0]
-            maximum_level = result[1].replace("/", "")  # 去掉第二个元素中的 "/"
-            my_style_levels[my_style_id] = [current_level, maximum_level]
+        my_style_num = SSR_style_num + SS_style_num
+        my_style_ids = SSR_style_ids + SS_style_ids
+        my_style_levels = add_dicts(SSR_style_levels, SS_style_levels)
+        limit_break_levels = add_dicts(SSR_limit_break_levels, SS_limit_break_levels)
+        my_style_infos = add_dicts(SSR_style_infos, SS_style_infos)
 
-            # 获取风格上限突破
-            limit_break_level = get_limit_break_level(style_item_card_elements, i)
-            limit_break_levels[my_style_id] = limit_break_level
+        print(f"[+] 拥有的 SSR 风格数: {len(SSR_style_ids)}")
+        print("[+] SSR 风格 ID:")
+        list_newline(SSR_style_ids, 6)
+        
+        print("[+] SSR 风格等级:")
+        dir_newline(SSR_style_levels, 3)
+        print("[+] SSR 风格上限突破:")
+        dir_newline(SSR_limit_break_levels, 6)
 
-            try:
-                # my_style_infos[my_style_id] = HBRbrochure.role_info.style_id_all_infos[my_style_id]
-                my_style_infos[my_style_id] = {}
-                my_style_infos[my_style_id]["current_level"] = current_level
-                my_style_infos[my_style_id]["maximum_level"] = maximum_level
-                my_style_infos[my_style_id]["limit_break_level"] = limit_break_level
-            except KeyError:
-                logger.error(f"[-] Missing information on style ID, style ID:   {my_style_id}")
-                print("[-] Missing information on style ID, style ID: " + my_style_id)
-                continue
 
-            my_style_num += 1
-
-        print(f"[+] 拥有的 SS 风格数: {len(my_style_ids)}")
+        print(f"[+] 拥有的 SS 风格数: {len(SS_style_ids)}")
         print("[+] SS 风格 ID:")
-        list_newline(my_style_ids, 6)
+        list_newline(SS_style_ids, 6)
         
         print("[+] SS 风格等级:")
-        dir_newline(my_style_levels, 3)
+        dir_newline(SS_style_levels, 3)
         print("[+] SS 风格上限突破:")
-        dir_newline(limit_break_levels, 6)
+        dir_newline(SS_limit_break_levels, 6)
 
         # 打开并切换到新标签页
         switch_to_brochure(chrome_driver, my_style_infos)
