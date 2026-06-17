c99a1ec661500f280ee1f9d76d9225565ece8f9e
CCELEND <CELEND2644884626@163.com>
2026-06-17 18:20:10 +0800

    update

--- 更新/check_proc.py
+++ 更新/check_proc.py
@@ -1,9 +1,10 @@
 
-from 更新.hash import calculate_file_hashes
+from 更新.hash import calculate_file_hashes, save_hashes_to_json
 from tkinter import messagebox
 import threading
 
 from 更新.http_client import send_hashes_to_server
+from tools import sort_dict_by_key
 
 from 日志.advanced_logger import AdvancedLogger
 logger = AdvancedLogger.get_logger(__name__)
@@ -24,8 +25,13 @@ def check_for_updates_proc():
     if 'files_to_download' in response:
         if response['files_to_download']:
             server_file_datetime = response.get('server_file_datetime', None)
+            server_file_hashes = response.get('server_file_hashes', None)
             print(f"[!] 检测到资源冲突或存在新版本，请更新！\n版本时间戳：{server_file_datetime}")
             messagebox.showinfo("提示", f"检测到资源冲突或存在新版本，请更新！\n版本时间戳：{server_file_datetime}")
+
+            if server_file_hashes:
+                server_file_hashes = sort_dict_by_key(server_file_hashes)
+                save_hashes_to_json(server_file_hashes, "./关于/server_file_hashes.json")
     else:
         messagebox.showerror("错误", f"错误响应：{response}\n请重试或联系开发者")
         logger.error(f"错误响应：{response}\n请重试或联系开发者")
