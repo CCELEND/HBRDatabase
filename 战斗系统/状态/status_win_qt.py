54c76b773a4ae29f2241b9798c41fa08dde274f2
CCELEND <CELEND2644884626@163.com>
2026-07-30 13:58:39 +0800

    update

--- 战斗系统/状态/status_win_qt.py
+++ 战斗系统/状态/status_win_qt.py
@@ -123,7 +123,7 @@ def show_statu(scrollbar_frame_obj):
                     bind_statu_canvas(statu_frame, statu, 0, 0)
 
                     if type_name in ['增益', '减益', '其他', '异常']:
-                        if series in ["技能效果强化", "对HP百分比伤害", "减益，异常移除", '强击破', 'EShield', '元素暴击伤害上升', '元素攻击上升', '元素暴击率上升', '连击数上升']:
+                        if series in ["技能效果强化", "对HP百分比伤害", "减益，异常移除", '强击破', 'EShield', '元素暴击伤害上升', '元素攻击上升', '元素暴击率上升', '连击数上升', '印记']:
                             statu_frame.setParent(series_frame)
                             statu_column_count = set_frame_newline(statu_frame, statu_num, 3, statu_column_count)
                         else:
