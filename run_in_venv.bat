06c2f9ae8e0e00a31d68fa777e3d0b2e165dfa86
CCELEND <CELEND2644884626@163.com>
2026-08-31 22:51:21 +0800

    自动请求管理员权限

--- run_in_venv.bat
+++ run_in_venv.bat
@@ -1,4 +1,11 @@
 @echo off
+:: 自动请求管理员权限
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Requesting administrator privileges...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
 setlocal enabledelayedexpansion
 cd /d "%~dp0"
 
